38b0a21c2d6327b836a83b387ad12ffee8b9ad3e
Matthew Zhang <zhangmatt22@gmail.com>
2025-02-27 18:24:29 -0600

    add feed screen and post-creating screen

--- app/frontend/LynxLiftsApp/App.tsx
+++ app/frontend/LynxLiftsApp/App.tsx
@@ -2,19 +2,33 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import RegisterScreen from './screens/RegisterScreen';
+import FeedScreen from './screens/FeedScreen';
+import CreatePostScreen from './screens/CreatePostScreen';
 
 const Stack = createStackNavigator();
 
+// const App = () => {
+//   return React.createElement(
+//     NavigationContainer,
+//     null,
+//     React.createElement(
+//       Stack.Navigator,
+//       null,
+//       //React.createElement(Stack.Screen, { name: 'Register', component: RegisterScreen }),
+//       React.createElement(Stack.Screen, { name: 'Feed', component: FeedScreen})
+//     )
+//   );
+// };
+
 const App = () => {
-  return React.createElement(
-    NavigationContainer,
-    null,
-    React.createElement(
-      Stack.Navigator,
-      null,
-      React.createElement(Stack.Screen, { name: 'Register', component: RegisterScreen })
-    )
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Feed">
+        <Stack.Screen name="Feed" component={FeedScreen} />
+        <Stack.Screen name="CreatePost" component={CreatePostScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
+}
 
 export default App;
