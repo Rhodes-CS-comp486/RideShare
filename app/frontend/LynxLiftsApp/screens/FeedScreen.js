b8bed0596e7aa3e13c7f969e6280c427181a9caf
Nalvi Tran <nalvkathe@gmail.com>
2025-03-04 13:09:41 -0600

    more UI fixes

--- app/frontend/LynxLiftsApp/screens/FeedScreen.js
+++ app/frontend/LynxLiftsApp/screens/FeedScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, Button, FlatList, StyleSheet, Image } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList, StyleSheet, Image } from 'react-native';
 // import Post from '../components/Post';
 
 
@@ -10,18 +10,24 @@ const FeedScreen = ({ navigation }) => {
   }
   return (
     <View style={styles.container}>
-            <Button title="Create Post" onPress={() => navigation.navigate('CreatePost', { addPost })} />
-            <FlatList
-                data={posts}
-                keyExtractor={(item, index) => index.toString()}
-                renderItem={({ item }) => (
-                    <View style={styles.post}>
-                        <Text>Pickup Time: {item.time}</Text>
-                        <Image></Image>
-                        <Text>{item.text}</Text>
-                    </View>
-                )}
-            />
+      <TouchableOpacity 
+        style={styles.createPostButton} 
+        onPress={() => navigation.navigate('CreatePost', { addPost })}
+      >
+        <Text style={styles.buttonText}>Create Post</Text>
+      </TouchableOpacity>
+
+      <FlatList
+        data={posts}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.post}>
+            <Text style={styles.postText}>Pickup Time: {item.time}</Text>
+            <Image></Image>
+            <Text style={styles.postText}>{item.text}</Text>
+          </View>
+        )}
+      />
         </View>
     );
 };
@@ -32,10 +38,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#80A1C2',
     padding: 20 
   },
-    post: { 
-      padding: 10, 
-      borderBottomWidth: 1, 
-      marginBottom: 10 }
+  createPostButton: {
+    backgroundColor: '#A62C2C',
+    padding: 12,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginBottom: 15,
+  },
+  buttonText: {
+    color: '#FAF2E6', 
+    fontSize: 16,
+  },
+  post: { 
+    padding: 10, 
+    borderBottomWidth: 1, 
+    borderBottomColor: '#6683A9',
+    marginBottom: 10 
+  },
+  postText: {
+    color: '#FAF2E6', 
+    fontSize: 16,
+  }
 });
 
 export default FeedScreen;
