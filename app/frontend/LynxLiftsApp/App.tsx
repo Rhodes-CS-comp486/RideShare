122aa78091cd4c97ed68c4b43505565f4bbb8be6
Nalvi Tran <nalvkathe@gmail.com>
2025-04-08 13:45:14 -0500

    swipe accept/decline posts

--- app/frontend/LynxLiftsApp/App.tsx
+++ app/frontend/LynxLiftsApp/App.tsx
@@ -1,6 +1,9 @@
+import 'react-native-reanimated';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 import RegisterScreen from './screens/RegisterScreen';
 import BufferScreen from './screens/BufferScreen';
 import FeedScreen from './screens/FeedScreen';
@@ -11,25 +14,28 @@ import ForgotPasswordScreen from './screens/ForgotPasswordScreen';
 import DriverFeedScreen from './screens/DriverFeedScreen';
 import DriverAccountScreen from './screens/DriverAccountScreen';
 import SetPreferenceScreen from './screens/SetPreferenceScreen';
+
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Welcome" component={BufferScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Feed" component={FeedScreen} />
-        <Stack.Screen name="Status" component={StatusScreen} />
-        <Stack.Screen name="Post" component={CreatePostScreen} />
-        <Stack.Screen name="Forgot" component={ForgotPasswordScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="DriverFeed" component={DriverFeedScreen} />
-        <Stack.Screen name="DriverAccount" component={DriverAccountScreen} />
-        <Stack.Screen name="SetPreference" component={SetPreferenceScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Welcome" component={BufferScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Feed" component={FeedScreen} />
+          <Stack.Screen name="Status" component={StatusScreen} />
+          <Stack.Screen name="Post" component={CreatePostScreen} />
+          <Stack.Screen name="Forgot" component={ForgotPasswordScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="DriverFeed" component={DriverFeedScreen} />
+          <Stack.Screen name="DriverAccount" component={DriverAccountScreen} />
+          <Stack.Screen name="SetPreference" component={SetPreferenceScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
-}
+};
 
 export default App;
